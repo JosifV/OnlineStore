570b43d72410991c0e92f51826ed35efd4ddfa04
JosifV <hatraga@gmail.com>
2018-12-17 13:51:58 +0100

    Add files via upload

---
+++ build/precache-manifest.835b62a642ee5d8136a4a7a768344efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "17bbd00482fcdbacd34155e3398ac84a",
+    "url": "/static/media/prodImg.17bbd004.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c7a1fc9d2e82da8bcd5",
+    "url": "/static/js/main.2c7a1fc9.chunk.js"
+  },
+  {
+    "revision": "b113e35caa62955c6175",
+    "url": "/static/js/1.b113e35c.chunk.js"
+  },
+  {
+    "revision": "2c7a1fc9d2e82da8bcd5",
+    "url": "/static/css/main.6688d984.chunk.css"
+  },
+  {
+    "revision": "b113e35caa62955c6175",
+    "url": "/static/css/1.b3f9a1b6.chunk.css"
+  },
+  {
+    "revision": "532a00e6acaf97f3c8babcb113af1335",
+    "url": "/index.html"
+  }
+];
